35673bf19b34c0816b275d19a95ea9895606475f
Julia Vu <juliadvu@gmail.com>
2020-06-15 01:53:15 -0500

    binary search test passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,8 @@
 def linear_search(arr, target):
     # Your code here
     for i in range(len(arr)):
-        if arr[i] == target:        #if array i value matches with the target value. then return the element i.
+        #if array i value matches with the target value. then return the element i.
+        if arr[i] == target:      
             return i    
 
     return -1   # Else not found. Similar to return None
@@ -11,6 +12,16 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    low = 0
+    high = len(arr) -1
 
-
+    while low <= high:
+        middle = (low+ high)//2
+        guess = arr[middle]     
+        if guess == target:
+            return middle
+        if guess > target:
+            high = middle -1
+        else:
+            low = middle +  1
     return -1  # not found
