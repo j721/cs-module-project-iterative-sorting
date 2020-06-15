a8773bb936c31e47132ca9e20bbaec2a23bcf1f1
Julia Vu <juliadvu@gmail.com>
2020-06-15 01:36:11 -0500

    added comments to linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,10 +1,10 @@
 def linear_search(arr, target):
     # Your code here
     for i in range(len(arr)):
-        if arr[i] == target:
+        if arr[i] == target:        #if array i value matches with the target value. then return the element i.
             return i    
 
-    return -1   # not found. Similar to return None
+    return -1   # Else not found. Similar to return None
 
 
 # Write an iterative implementation of Binary Search
